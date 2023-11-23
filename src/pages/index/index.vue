baa91a6b780868390298376a8311596b64a8f2b0
“damoncai” <782099197@qq.com>
2023-11-23 18:17:21 +0800

    初始化工程

---
+++ src/pages/index/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+//
+</script>
+
+<template>
+  <view>
+    <uni-calendar :insert="true" :lunar="true" :start-date="'2019-3-2'" :end-date="'2019-5-20'" @change="change" />
+  </view>
+</template>
+
+<style lang="scss">
+//
+</style>
